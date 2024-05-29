81c1ba6ec07761b3bc926f1f792042c221a5774c
minthukha <minthukha1239788@gmail.com>
2024-05-29 14:57:55 +0630

    Add project files.

---
+++ SerilogExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Debug()
+        .WriteTo.Console()
+        .WriteTo.File("logs/SerilogExample.txt", rollingInterval: RollingInterval.Hour)
+        .CreateLogger();
+
+try
+{
+    Log.Information("Application Starting");
+
+    int a = 10, b = 0;
+    Log.Debug("Attempting to divide {A} by {B}", a, b);
+
+    Console.WriteLine(a / b);
+}
+catch (Exception ex)
+{
+    Log.Warning(ex, "An error occurred while dividing numbers");
+}
+finally
+{
+    Log.Information("Application Ending");
+    await Log.CloseAndFlushAsync();
+}
+
+Console.ReadKey();
